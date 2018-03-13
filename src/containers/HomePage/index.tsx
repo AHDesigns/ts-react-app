1aef141568215afc9dc8b4b9e6467d8ee1fa05ac
Adam <adam.hall@sky.uk>
2018-03-13 00:43:03 +0000

    added some components & containers

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -5,9 +5,9 @@ import Column from '../../components/Column';
 
 interface Idetails { name: string; }
 
-export interface Iticket { name: string; body: string; }
+export interface Iticket { key?: number; name: string; body: string; }
 
-export interface Icolumn { details: Idetails; tickets: Iticket[]; }
+export interface Icolumn { key?: string; details: Idetails; tickets: Iticket[]; }
 
 export default class HomePage extends React.PureComponent {
   render() {
@@ -40,7 +40,7 @@ export default class HomePage extends React.PureComponent {
     ];
 
     const columns = allColumns.map((column: Icolumn) => (
-        <Column {...column} />
+        <Column key={column.details.name} {...column} />
     ));
 
     return (
