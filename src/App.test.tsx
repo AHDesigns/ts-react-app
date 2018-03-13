3335d98bcf0a392d74d873181c1e665c0ccdf732
Adam <adam.hall@sky.uk>
2018-03-13 00:32:07 +0000

    added setupTests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 import App from './App';
-import * as Adapter from 'enzyme-adapter-react-16';
-
-enzyme.configure({ adapter: new Adapter() });
 
 it('renders the correct text when no enthusiasm level is given', () => {
         const hello = enzyme.shallow(<App name="Daniel" />);
