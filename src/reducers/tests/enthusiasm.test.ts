61b8205aeb6df60dd63b3da2892dc22b5e30c574
Adam <adam.hall@sky.uk>
2018-03-18 11:58:02 +0000

    added a test

---
+++ src/reducers/tests/enthusiasm.test.ts
@@ -0,0 +1,22 @@
+import { enthusiasm, initialState } from '../enthusiasm';
+import { incrementEnthusiasm, decrementEnthusiasm } from '../../actions';
+
+describe('enthusiasm', () => {
+    describe('initial state', () => {
+        it('should match a snapshot', () => {
+            expect(initialState).toEqual({
+                enthusiasmLevel: 5
+            });
+        });
+    });
+
+    describe('incrementEnthusiasm', () => {
+        it('it should add 1', () => {
+            const action = incrementEnthusiasm();
+            const state = enthusiasm(initialState, action);
+            expect(state.enthusiasmLevel).toBe(6);
+        });
+    });
+
+    // etc...
+});
